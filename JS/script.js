e59a06c089828f6f5478a17abd7e0eb40830ab8c
Mananofficial <manansharma.mail@gmail.com>
2025-09-05 02:57:34 +0530

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -26,7 +26,7 @@ function formatSeconds(seconds, showMilliseconds = false) {
 
 async function getSongs(folder) {
     currfolder = folder;
-    let a = await fetch(`http://127.0.0.1:3000/${folder}/`)
+    let a = await fetch(`/${folder}/`)
     let response = await a.text();
     let div = document.createElement("div")
     div.innerHTML = response;
@@ -76,7 +76,7 @@ const playmusic = (track, pause = false) => {
 }
 
 async function displayalbums() {
-    let a = await fetch(`http://127.0.0.1:3000/songs/`)
+    let a = await fetch(`/songs/`)
     let response = await a.text();
     let div = document.createElement("div")
     div.innerHTML = response;
@@ -89,7 +89,7 @@ async function displayalbums() {
         if (e.href.includes("/songs")) {
             let folder = e.href.split("/").slice(-2)[0]
             
-            let a = await fetch(`http://127.0.0.1:3000/songs/${folder}/info.json`)
+            let a = await fetch(`/songs/${folder}/info.json`)
             let response = await a.json();
             cardContainer.innerHTML = cardContainer.innerHTML + `<div data-folder="${folder}" class="card">
             <div class="play">
@@ -188,5 +188,6 @@ async function main() {
         }
     })
 }
+
 
 main()
